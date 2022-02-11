b383276e07643c4a13394df7e293d366e26ff5ee
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-02-10 21:55:37 -0600

    cleaned up js

--- script.js
+++ script.js
@@ -1,70 +1,10 @@
-const tl = gsap.timeline({
-	defaults: { ease: 'power4.inOut' },
-});
+const tl = gsap.timeline({ defaults: { ease: 'power4.inOut' } });
 
-tl.to('nav', {
-	y: 0,
-})
-	.to(
-		'footer',
-		{
-			y: 0,
-		},
-		'-=.5',
-	)
-	.to(
-		'header',
-		{
-			opacity: 1,
-			duration: 1.5,
-		},
-		'-=.5',
-	)
-	.to(
-		'.banner',
-		{
-			y: 0,
-			opacity: 1,
-			duration: 2,
-		},
-		'-=1.5',
-	)
-	.to(
-		'.banner h3',
-		{
-			y: 0,
-			opacity: 1,
-			duration: 1.6,
-		},
-		'-=1.5',
-	)
-	.to(
-		'.column:nth-child(1)',
-		{
-			x: 0,
-			scale: 1,
-			duration: 1,
-			opacity: 1,
-		},
-		'-=1',
-	)
-	.to(
-		'.column:nth-child(2)',
-		{
-			x: 0,
-			scale: 1,
-			duration: 1,
-			opacity: 1,
-		},
-		'-=1',
-	)
-	.to(
-		'.box img',
-		{
-			y: 0,
-			opacity: 1,
-			duration: 0.8,
-			stagger: 0.3,
-		},
-		'-=1',
-	);
+tl.to('nav', { y: 0 })
+	.to('footer', { y: 0 }, '-=.5')
+	.to('header', { opacity: 1, duration: 1.5 }, '-=.5')
+	.to('.banner', { y: 0, opacity: 1, duration: 2 }, '-=1.5')
+	.to('.banner h3', { y: 0, opacity: 1, duration: 1.6 }, '-=1.5')
+	.to('.column', { x: 0, scale: 1, duration: 1, opacity: 1 }, '-=1')
+	// .to('.column:nth-child(2)',{ x: 0, scale: 1, duration: 1, opacity: 1 }, '-=1')
+	.to('.box img', { y: 0, opacity: 1, duration: 0.8, stagger: 0.3 }, '-=1');
